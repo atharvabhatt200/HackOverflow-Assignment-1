fff0f3d448f37b8e0cb200b4383c35ae2b92fac9
atharvabhatt200 <atharvabhatt200@gmail.com>
2022-03-02 22:52:18 +0530

    Make a rgb color picker game - HackOverflow Assignment 1.

---
+++ README.md
@@ -0,0 +1,17 @@
+# HackOverflow Assignment 1
+
+Welcome to RGB Color Picker Game!
+
+### Tutorial:
+- The game will give you an option to choose either Easy or Hard Mode.
+- The Easy Mode will consist of 3 tiles, and the hard mode will consist of 6 tiles.
+- The game will display a random RGB value on the top of the screen. 
+- One of the colored tiles will be of the correct RGB value as displayed on the top. The rest of the tiles will be of random RGB values. 
+- On selecting the colored card that matches the RGB code displayed on top, you win the game. 
+- If the selected tile is wrong, it will be elliminated.
+- Click on New Colors or Play Again to start a new game.
+- You can chosse difficulty using Choose Level option.
+
+The link to the deployment is: 
+
+Have fun!
---
+++ main.js
@@ -0,0 +1,135 @@
+var boxes = document.querySelectorAll(".box");
+var rgbspan = document.querySelector('.rgbspan');
+var colors = generateRandomColor(6);
+var pickedColor = colors[Math.floor(Math.random() * 6)];
+var playbtn = document.querySelector("#playAgain");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+var boxCount = 6;
+var statusText = document.querySelector(".status");
+var startMenu = document.querySelector(".startMenu");
+var line = document.querySelector(".line");
+var mainBox = document.querySelector(".mainBox");
+var reload = document.querySelector("#reload");
+
+statusText.textContent = "";
+rgbspan.textContent = pickedColor;
+
+easyBtn.addEventListener("click", function() {
+    
+    startMenu.style.display = "none";
+    line.style.display = "block";
+    mainBox.style.display = "block";
+    document.querySelector("h1").style.background = "#ff1e00";
+
+    statusText.textContent = "";
+
+    boxCount = 3;
+
+    colors = generateRandomColor(boxCount);
+    pickedColor = colors[Math.floor(Math.random() * 3)];
+    rgbspan.textContent = pickedColor;
+
+    for (var i = 0; i < boxes.length; i++) {
+        if (colors[i]) {
+            boxes[i].style.background = colors[i];
+        } else {
+            boxes[i].style.display = "none";
+        }
+    }
+});
+
+
+hardBtn.addEventListener("click", function() {
+    
+    startMenu.style.display = "none";
+    line.style.display = "block";
+    mainBox.style.display = "block";
+
+    document.querySelector("h1").style.background = "#15011a";
+
+    statusText.textContent = "";
+
+    boxCount = 6;
+    colors = generateRandomColor(boxCount);
+    pickedColor = colors[Math.floor(Math.random() * 6)];
+    console.log(boxes.length)
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].style.background = colors[i];
+        boxes[i].style.display = "block";
+
+    }
+});
+
+
+playbtn.addEventListener("click", function() {
+    document.querySelector("h1").style.background = "#66011a";
+
+    playbtn.textContent = "New Colors";
+    statusText.textContent = "";
+
+    colors = generateRandomColor(boxCount);
+    pickedColor = colors[Math.floor(Math.random() * boxCount)];
+    rgbspan.textContent = pickedColor;
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].style.borderColor = "black";
+        boxes[i].style.background = colors[i];
+    }
+});
+
+reload.addEventListener("click", function(){
+    location.reload();
+});
+
+for (var i = 0; i < colors.length; i++) {
+    boxes[i].style.borderColor = "black";
+    boxes[i].style.background = colors[i];
+    boxes[i].addEventListener('click', function() {
+        var selectedColor = this.style.background;
+        console.log(selectedColor);
+        console.log(pickedColor);
+        if (selectedColor === pickedColor) {
+            win();
+
+        } else {
+            loose(this);
+        }
+    });
+}
+
+
+function win() {
+    for (var i = 0; i < colors.length; i++) {
+        boxes[i].style.background = pickedColor;
+        boxes[i].style.borderColor = "black";
+    }
+    document.querySelector("h1").style.background = pickedColor;
+    playbtn.textContent = "Play Again!!";
+    statusText.textContent = "Correct!!";
+
+}
+
+
+function loose(a) {
+    console.log(a);
+    a.style.background = "transparent";
+    a.style.borderColor = "transparent";
+    statusText.textContent = "Try Again!";
+}
+
+
+function generateRandomColor(num) {
+    var arr = []
+    for (var i = 0; i < num; i++) {
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
